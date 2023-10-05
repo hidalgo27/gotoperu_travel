b0eab1931974a66f029aeca4d3f4db10be56d5e5
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2023-10-05 10:07:29 -0500

    Base de la aplicacion creada

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   apps: [
     {
       name: 'NuxtAppName',
-      port: '3000',
+      port: '3001',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs',
