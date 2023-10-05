03eab1a83f222caf3c21ff2a8fcfcb2cb571953f
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2023-10-05 09:34:36 -0500

    Base de la aplicacion creada

---
+++ components/page/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="bg-gray-800 py-8 text-secondary text-center text-sm">
+<!--    <nuxt-link to="/"><img src="/images/logos/logo-gotolatam.svg" class="w-[180px] md:w-[240px] mx-auto"></nuxt-link>-->
+    © Copyright 2023 GotoLATAM. All rights reserved.
+  </footer>
+</template>
+
+<script>
+</script>
