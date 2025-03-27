a88f986cd5b631062103a89ff4e6e1e3de0ca207
Julio <julio@goto.group>
2025-03-27 15:31:54 -0500

    feat: Add Banner of Promotions

---
+++ components/page/PromoBanner.vue
@@ -0,0 +1,28 @@
+<script setup>
+const { $gsap } = useNuxtApp();
+
+const isVisible = ref(true);
+
+const closeBanner = () => {
+  $gsap.to('.bannerPromo', {
+    opacity: 0,
+    y: -50,
+    duration: 0.5,
+    onComplete: () => (isVisible.value = false),
+  });
+};
+
+onMounted(() => {
+  $gsap.fromTo('.bannerPromo', { opacity: 0, y: -50, duration: 0.5, ease: 'power3.in' }, { opacity: 1, y: 0, duration: 0.5, ease: 'power3.out' });
+});
+</script>
+
+<template>
+  <div v-if="isVisible"
+    class="bannerPromo w-full bg-secondary hover:bg-opacity-50 duration-500 transition text-black text-center font-bold p-3 flex justify-between items-center">
+    <p class="flex-1 text-center">FLASH SALE NOW ON! Save up to 20%. <NuxtLink class="underline cursor-pointer">View
+        deals.</NuxtLink>
+    </p>
+    <button @click="closeBanner" class="mr-4 text-xl hover:text-gray-700">✕</button>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,19 @@
-<template>
-<!--  <div class="tpl-snow">-->
-<!--    <div v-for="index in 50" :key="index"></div>-->
-<!--  </div>-->
+<script lang="ts" setup>
+import Footer from "~/components/page/Footer.vue";
+import Headers from "~/components/page/Headers.vue";
+import ModalMenu from "~/components/modal/ModalMenu.vue";
+import FooterInquire from "~/components/form/FooterInquire.vue";
+import Float from "~/components/page/Float.vue";
+import PromoBanner from "~/components/page/PromoBanner.vue";
+</script>
 
-  <Headers></Headers>
+<template>
+  <!--  <div class="tpl-snow">-->
+  <!--    <div v-for="index in 50" :key="index"></div>-->
+  <!--  </div>-->
+  <PromoBanner></PromoBanner>
+  <Headers>
+  </Headers>
 
   <slot />
 
@@ -13,11 +23,3 @@
   <Footer></Footer>
   <Float></Float>
 </template>
-
-<script lang="ts" setup >
-import Footer from "~/components/page/Footer.vue";
-import Headers from "~/components/page/Headers.vue";
-import ModalMenu from "~/components/modal/ModalMenu.vue";
-import FooterInquire from "~/components/form/FooterInquire.vue";
-import Float from "~/components/page/Float.vue";
-</script>
